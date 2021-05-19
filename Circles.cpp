88398bca2f2d0078be143d375b8dc951ab52e8b1
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2021-05-19 15:31:27 -0500

    Add files via upload

---
+++ Circles.cpp
@@ -0,0 +1,42 @@
+/*
+Circles
+Name
+Date
+This program has a class called Circle, which is used
+to represent circles. Currently, the class is missing
+two methods: calculateArea() and calculateCircumference().
+The first, calculateArea(), should calculate the area of
+a circle. The second, caluclateCircumference(), should
+calculate the circumference (perimeter) of a circle. Write
+these two methods.
+Once you have written the methods, create two Circle objects
+in main(), cirlce1 and circle2, with a radius of 4 and a
+radius of 5.5 respectively. Call both the calculateArea() and
+calculateCircumference() methods for each object and display
+the results in the console.
+*/
+#include <iostream>
+using namespace std;
+
+class Circle
+{
+private:
+    double radius;
+
+public:
+    Circle(double radius)
+    {
+        this->radius = radius;
+    }
+    /*
+    Write your calculateArea() and calculateCircumference()
+    methods here!
+    */
+};
+
+int main()
+{
+    /*
+    Create your objects and call the methods here!
+    */
+}
